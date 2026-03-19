7036123170d7c3874fdaeef49b224ff893746002
LordShangbin <89254359+LordShangbin@users.noreply.github.com>
2026-03-19 17:11:56 +0700

    Example update1

--- DP_2D_test.cpp
+++ DP_2D_test.cpp
@@ -8,19 +8,21 @@ int n, m;
 int main(){
     cin.tie(nullptr)->sync_with_stdio(0);
     cin >> n >> m;
-    vector<vector<int>> dp(n+1, vector<int>(m+1));
-    init(dp, {1, n+1}, {1, m+1});
+    vector<vector<int>> dp(n+1, vector<int>(m+1)); // Define dp (Vector only)
+    init(dp, {1, n+1}, {1, m+1}); //Init --> (dp, printing_range_i, printing_range_j)
     dp[0][1] = 1;
-    Describe("dp[0] = 1");
-    Visualize(dp, {}, {});
+    Describe("dp[0] = 1"); //Description
+    Visualize(dp, {}, {}); //Visualize
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= m; j++){
             dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % modnum;
-            Describe("dp[i] = %d + %d", dp[i - 1][j], dp[i][j - 1]);
-            Visualize(dp, {{i - 1, j}, {i, j - 1}}, {{i, j}});
+            Describe("dp[i] = %d + %d", dp[i - 1][j], dp[i][j - 1]); //Description
+            vector<pair<int, int>> purple = {{i - 1, j}, {i, j - 1}};
+            vector<pair<int, int>> green = {{i, j}};
+            Visualize(dp, purple, green); 
         }
     }
     cout << dp[n][m];
-    while(!WindowShouldClose()) Visualize(dp, {}, {});
-    stop();
+    while(!WindowShouldClose()) Visualize(dp, {}, {}); //ต้องใส่
+    stop(); //ต้องใส่
 }
